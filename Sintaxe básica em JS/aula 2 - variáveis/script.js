9a90eefd76ac6b3d801b26db9d7866ffbc3a3dae
Filipe Mello <116228977+FilipeMell0@users.noreply.github.com>
2023-08-23 20:32:31 -0300

    Adicionando os arquivos

---
+++ Sintaxe básica em JS/aula 2 - variáveis/script.js
@@ -0,0 +1,84 @@
+// tipode de primitivos
+
+// boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'filipe';
+console.log(typeof(nome));
+
+// como declarar 
+var variavel= 'filipe';
+variavel = 'mello';
+console.log(variavel);
+
+let variavel2 = 'filipe';
+variavel2 = 'mello';
+console.log(variavel2);
+
+const constante = 'filipe';
+console.log(constante);
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+console.log(escopoLocal);
+
+// adição
+var adição = 1 + 1;
+console.log(adição);
+
+//  subtração
+var subtração = 2 - 1;
+console.log(subtração);
+
+// multiplicação
+var multiplicação = 2 * 3;
+console.log(multiplicação);
+
+// divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// pontenciação
+var pontenciação = 2 ** 10;
+console.log(pontenciação);
+
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a 
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a
+var menouOuIgual = 5 <= 2;
+console.log(menorQue);*/
+
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
---
+++ Sintaxe básica em JS/aula 6 - aprofundando em funções/tiposDeFuncao.js
@@ -0,0 +1,23 @@
+// tipós de função
+
+// declarativas
+/*function funcao() {
+    console.log('Sou uma mensagem de uma função declarativa')
+}
+
+funcao();*/
+
+// expressão de função
+// com nomeação
+/*var funcao = function () {
+    console.log('Sou uma mensagem de função de expressão!')
+}
+
+funcao();*/
+
+// arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+
+funcao();
